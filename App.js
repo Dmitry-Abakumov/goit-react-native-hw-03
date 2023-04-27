83cc3c0323099271d8a32a674d369729a002169e
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-04-27 20:22:05 +0200

    bug fix

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { useEffect, useCallback } from "react";
 import FinalScreen from "./Screens/FinalScreen";
 // import useAsyncLoadFonts from "./shared/hooks/useAsyncLoadFonts";
 
-// SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(true);
@@ -17,24 +17,11 @@ export default function App() {
     "Roboto-Regular": require("./shared/fonts/Roboto-Regular.ttf"),
   });
 
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
   useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
 
-    prepare();
-  }, []);
-
-  if (!fontsLoaded) {
-    return null;
-  } else {
-    SplashScreen.hideAsync();
-  }
+  if (!fontsLoaded) return null;
 
   return (
     <NavigationContainer>
