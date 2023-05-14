ada03d8beb7fcf08d2381a03c5df780a6ecc7d0e
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-05-14 21:09:35 +0200

    bug fix

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,8 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { authSlice } from "./auth/authSlice";
-
-rootReducer = combineReducers({ [authSlice.name]: authSlice.reducer });
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth/authSlice";
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: authReducer,
+  },
 });
